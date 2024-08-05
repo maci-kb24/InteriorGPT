782db875a474e8086a8977bf89397fa940ff1d63
maci-kb24 <macikb8@yahoo.com>
2024-08-05 15:08:59 +0200

    edit the .env to .env.local

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upcdn.io", "replicate.delivery"],
+    domains: ["upcdn.io", "replicate.delivery", "interiorgpt.cc/generate"],
   },
 };
